2ddcf4559cdc4bf7fd4fd01d658892cf85c0919b
dmp42 <olivier@webitup.fr>
2014-03-15 12:23:04 +0100

    - move jshint.json to the default .jshintrc (should fix #82) - remove eslint reporter dep (since stylish is the default now) - silence out mocha / switch reporter

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,6 @@
     var jshint = require('gulp-jshint');
 
     var jsreporter = require('jshint-stylish');
-    // var esreporter = require('eslint-stylish');
-
-    var fs = require('fs');
 
     var tests = ['./test/**/*.js'];
     var scripts = ['*.js', './bin/*', './lib/*.js', './test/*.js'];
@@ -20,26 +17,24 @@
 
     gulp.task('mocha', function () {
         return gulp.src(tests)
-            .pipe(mocha({reporter: 'nyan'}));
+            .pipe(mocha({ reporter: 'spec' }));
     });
 
     gulp.task("mocha|catch", function () {
         return gulp.src(tests)
-            .pipe(mocha({ reporter: 'spec' }).on("error", function (err) {
-                console.log(err.toString());
+            .pipe(mocha({ reporter: 'spec' }).on("error", function (/*err*/) {
                 this.emit('end');
             })
         );
     });
 
-    var jsconfig = JSON.parse(fs.readFileSync('./jshint.json')); // .jshintrc'));
     // var esconfig = JSON.parse(fs.readFileSync('./.eslintrc'));
 
     gulp.task('hint', function () {
         gulp.src(scripts)
             // eslint(esconfig),
             // eslint.formatEach(esreporter),
-            .pipe(jshint(jsconfig))
+            .pipe(jshint())
             .pipe(jshint.reporter(jsreporter));
     });
 
