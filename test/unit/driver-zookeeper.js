549e0c973e6a16684082ca669895780a3b9e447c
Olivier Gambier <olivier@docker.com>
2015-01-06 19:23:04 -0800

    Fix zookeeper

--- test/unit/driver-zookeeper.js
+++ test/unit/driver-zookeeper.js
@@ -4,7 +4,8 @@
 
     var npmlog = require('npmlog');
     var fs = require('fs');
-    var spawn = require('child_process').spawn;
+    var os = require('os');
+    var rimraf = require('rimraf');
 
     if (process.env.NO_ZOOKEEPER) {
         npmlog.error('Test', 'No zookeeper server on this machine! No tests, then.');
@@ -12,7 +13,7 @@
     }
 
     // Useful if you want to see servers talk to you
-    //require('npmlog').level = 'silly';
+    // require('npmlog').level = 'silly';
 
     // var expect = require('chai').expect;
 
@@ -21,24 +22,30 @@
     var testReading = require('./driver-test-reading');
 
     var s1 = new Server();
+    var serverTemp = os.tmpdir() + '/hipache-zookeeper-test';
+    process.env.ZOO_LOG_DIR = serverTemp;
+    process.env.ZOOPIDFILE = serverTemp + '.pid';
 
     before(function (done) {
-        fs.writeFile(process.env.HOME + '/zookeeper/zoo.cfg', ['tickTime=2000',
-            'dataDir=' + process.env.HOME + '/zookeeper/data/', 'clientPort=2182'].join('\n'), function(err) {
+        if (!fs.existsSync(serverTemp)){
+            fs.mkdirSync(serverTemp);
+        }
+        fs.writeFile(serverTemp + '/zoo.cfg', ['tickTime=1000',
+            'dataDir=' + serverTemp + '/data/', 'clientPort=2182'].join('\n'), function(err) {
             if(err) {
                 npmlog.error('Test', 'Error writing zookeeper test config: ' + err);
+                return;
             }
+            // Zookeeper needs time to take-off the ground :(
+            s1.start([serverTemp + '/zoo.cfg']).once('stopped', function () {
+                done();
+            });
         });
-        process.env.ZOO_LOG_DIR = process.env.HOME + '/zookeeper/';
-
-        // Zookeeper needs time to take-off the ground :(
-        s1.start(['start-foreground', process.env.HOME + '/zookeeper/zoo.cfg']).once('started', done);
     });
 
     after(function (done) {
-        s1.stop().once('stopped', function(){
-          spawn('/bin/rm', ['-rf', process.env.HOME + '/zookeeper/data/']);
-          done();
+        s1.stop().once('stopped', function () {
+            rimraf(serverTemp, done);
         });
     });
 
